1f741c4df1ae36eabfee3508ad9585c646243e25
linearesdiego <diegolineares@gmail.com>
2024-11-27 10:58:32 -0300

    a

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const productoRoutes = require('./routes/productoRoutes');
 const pedidoRoutes = require('./routes/pedidoRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const authRoutes = require('./routes/authRoutes');
+
 require('dotenv').config();
 
 const app = express();
@@ -26,26 +27,7 @@ app.use((err, req, res, next) => {
 // Sincronizar la base de datos y arrancar el servidor
 const PORT = process.env.DB_PORT || 3000;
 
-// Sincronizar la tabla Categoria primero y luego el resto
-sequelize.sync({ alter: true })
-    .then(async () => {
-        // Sincroniza la tabla Categoria primero
-        await require('./models').Categoria.sync({ force: true });
-        console.log('Tabla Categoria creada o sincronizada.');
-
-        // Luego sincroniza las demás tablas
-        await require('./models').Producto.sync({ force: true });
-        console.log('Tabla Producto creada o sincronizada.');
-
-        await require('./models').Pedido.sync({ force: true });
-        console.log('Tabla Pedido creada o sincronizada.');
-
-        await require('./models').PedidoProducto.sync({ force: true });
-        console.log('Tabla PedidoProducto creada o sincronizada.');
-
-        // Arrancar el servidor después de la sincronización
-        app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-    })
-    .catch(error => {
-        console.error('Error al sincronizar las tablas:', error);
-    });
+sequelize.sync({ /* alter: true  */}).then(() => {
+    console.log('Base de datos sincronizada');
+    app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+});
