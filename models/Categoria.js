6c573cf2413ec42a65200369f7519d362a0b9725
linearesdiego <diegolineares@gmail.com>
2024-11-27 10:47:33 -0300

    a

--- models/Categoria.js
+++ models/Categoria.js
@@ -2,8 +2,8 @@ module.exports = (sequelize, DataTypes) => {
     const Categoria = sequelize.define('Categoria', {
       id: {
         type: DataTypes.INTEGER,
-        primaryKey: true, // Especifica que este campo es la clave primaria
-        autoIncrement: true, // Hace que el campo se incremente automáticamente
+        primaryKey: true,
+        autoIncrement: true, 
       },
       nombre: {
         type: DataTypes.STRING,
@@ -16,8 +16,8 @@ module.exports = (sequelize, DataTypes) => {
     });
     Categoria.associate = (models) => {
       Categoria.hasMany(models.Producto, {
-        foreignKey: 'CategoriaId', // La clave foránea en Producto
-        as: 'productos', // El alias que se usará para acceder a los productos de una categoría
+        foreignKey: 'CategoriaId', 
+        as: 'productos', 
       });
     };
     return Categoria;
